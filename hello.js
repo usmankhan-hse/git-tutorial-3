fc08c9852bcf282e6986fd83eb64e31349b8ee67
usmankhan-hse <usmankhan.hse@gmail.com>
2025-06-09 13:52:38 +0400

    first commit

---
+++ hello.js
@@ -0,0 +1,11 @@
+console.log('Hello git');
+console.log('Hello github');
+console.log('hello gitbucket');
+console.log('hello from dev-adrian');
+console.log('This is bad code');
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Hellooo, git
+## Welcome to Git!
+ Yo, Johnny here
+ - This is coming from `dev-adrian`
+- i am adding this from 'feature-branch'
